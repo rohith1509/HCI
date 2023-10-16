a7fe0ecc7f2aec10eac68d282f95f8154d461e33
rohith1509 <67113455+rohith1509@users.noreply.github.com>
2023-10-16 13:53:09 -0400

    Update scrip1.js

--- scrip1.js
+++ scrip1.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     document.querySelectorAll('.recipe img').forEach(function(img) {
         img.addEventListener('click', function() {
             var recipeNumber = this.parentElement.id.replace('recipe', '');
-            window.location.href = 'C:/Users/panch/Desktop/test/recipe1.html';
+            window.location.href = 'recipe1.html';
         });
     });
 });
